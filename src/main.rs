d9b5e0d45e46c7b8b066dd508306f4a19c2e6c97
karuha <1402125581@qq.com>
2024-09-03 11:35:25 +0800

    fix data format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use dora_node_api::arrow::datatypes::ToByteSlice;
-use dora_node_api::{DoraNode, Event};
+use dora_node_api::{ArrowData, DoraNode, Event};
 use std::io::Write;
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 fn main() -> eyre::Result<()> {
     let (sx, rx) = mpsc::channel();
@@ -21,25 +22,25 @@ fn main() -> eyre::Result<()> {
         };
 
         match event {
-            Event::Input { ref data, .. } => {
-                let bytes = data.try_into().unwrap();
-                let _ = sx.send(bytes);
+            Event::Input { data, .. } => {
+                let _ = sx.send(data);
             }
             Event::Stop => println!("Received manual stop"),
             other => eprintln!("Received unexpected input: {other:?}"),
         }
     }
+    // thread::sleep(Duration::from_secs(5));
 
     eyre::Ok(())
 }
 
-fn forward_server(receiver: mpsc::Receiver<Vec<u8>>) {
+fn forward_server(receiver: mpsc::Receiver<ArrowData>) {
     let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         loop {
             let data = receiver.recv().unwrap();
-            stream.write(data.to_byte_slice()).unwrap();
+            stream.write((&data).try_into().unwrap()).unwrap();
         }
     }
 }
